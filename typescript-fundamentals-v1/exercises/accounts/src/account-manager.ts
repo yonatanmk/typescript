3d96ffb9fad778229f4ebbef1d3effbe60bbd673
yonatanmk <yonatanmk@gmail.com>
2020-10-29 11:30:45 -0400

    sec 1 to 4

--- typescript-fundamentals-v1/exercises/accounts/src/account-manager.ts
+++ typescript-fundamentals-v1/exercises/accounts/src/account-manager.ts
@@ -1,5 +1,23 @@
+interface User {
+  email: string;
+  password: string;
+}
+
+interface ConfirmedUser {
+  email: string;
+  password: string;
+  isActive: boolean;
+}
+
+interface Admin {
+  email: string;
+  password: string;
+  isActive: boolean;
+  adminSince: Date;
+}
+
 export class AccountManager {
-  users = new Array();
+  users: User[] = new Array();
 
   /**
    * Create a new user account
@@ -8,7 +26,7 @@ export class AccountManager {
    * @return the new user account. An admin must activate it using activateNewUser
    * @see this.activateNewUser
    */
-  register(email, password) {
+  register(email: string, password: string): User {
     if(!email) throw 'Must provide an email';
     if(!password) throw 'Must provide a password';
     let user = { email, password };
@@ -22,10 +40,11 @@ export class AccountManager {
    * @param userToApprove Newly-registered user, who is to be activated
    * @return the updated user object, now activated
    */
-  activateNewUser(approver, userToApprove) {
+  activateNewUser(approver: Admin, userToApprove: User): ConfirmedUser {
     if (!approver.adminSince) throw "Approver is not an admin!";
-    userToApprove.isActive = true;
-    return userToApprove;
+    let toConfirm = userToApprove as ConfirmedUser
+    toConfirm.isActive = true;
+    return toConfirm;
   }
 
   /**
@@ -34,10 +53,11 @@ export class AccountManager {
    * @param user an active user who you're making an admin
    * @return the updated user object, now can also be regarded as an admin
    */
-  promoteToAdmin(existingAdmin, user) {
+  promoteToAdmin(existingAdmin: Admin, user: ConfirmedUser): Admin {
     if (!existingAdmin.adminSince) throw "Not an admin!";
     if (user.isActive !== true) throw "User must be active in order to be promoted to admin!";
-    user.adminSince = new Date();
-    return user;
+    let newAdmin = user as Admin
+    newAdmin.adminSince = new Date();
+    return newAdmin;
   }
 }
--- typescript-fundamentals-v2/notes/1-basics.ts
+++ typescript-fundamentals-v2/notes/1-basics.ts
@@ -3,22 +3,22 @@
 /**
  * (1) x is a string, b/c we’ve initialized it
  */
-// let x = "hello world";
+let x = "hello world";
 
 /**
  * (2) reassignment is fine
  */
-// x = "hello mars";
+x = "hello mars";
 
 /**
  * (3) but if we try to change type
  */
-// x = 42; // 🚨 ERROR
+x = 42; // 🚨 ERROR
 
 /**
  * (4) let's look at const. The type is literally 'hello world'
  */
-// const y = "hello world";
+const y = "hello world";
 
 /**
  * This is called a 'string literal type'. y can never be reassigned since it's a const,
@@ -51,27 +51,29 @@
 /**
  * (8) simple array types can be expressed using []
  */
-// let aa: number[] = [];
-// aa.push(33);
-// aa.push("abc"); // 🚨 ERROR: Argument of type '"abc"' is not assignable to parameter of type 'number'.
+let aa: number[] = [];
+aa.push(33);
+aa.push("abc"); // 🚨 ERROR: Argument of type '"abc"' is not assignable to parameter of type 'number'.
+
+let bb: (string | number)[] = [];
 
 /**
  * (9) we can even define a tuple, which has a fixed length
  */
-// let bb: [number, string, string, number] = [
-//   123,
-//   "Fake Street",
-//   "Nowhere, USA",
-//   10110
-// ];
+let bb: [number, string, string, number] = [
+  123,
+  "Fake Street",
+  "Nowhere, USA",
+  10110
+];
 
-// bb = [1, 2, 3]; // 🚨 ERROR: Type 'number' is not assignable to type 'string'.
+bb = [1, 2, 3]; // 🚨 ERROR: Type 'number' is not assignable to type 'string'.
 
 /**
  * (10) Tuple values often require type annotations (  : [number, number] )
  */
-// const xx = [32, 31]; // number[];
-// const yy: [number, number] = [32, 31];
+const xx = [32, 31]; // number[];
+const yy: [number, number] = [32, 31];
 
 //== OBJECTS ==//
 
@@ -117,44 +119,44 @@
  * Sometimes we have a type that can be one of several things
  */
 
-// export interface HasPhoneNumber {
-//   name: string;
-//   phone: number;
-// }
+export interface HasPhoneNumber {
+  name: string;
+  phone: number;
+}
 
-// export interface HasEmail {
-//   name: string;
-//   email: string;
-// }
+export interface HasEmail {
+  name: string;
+  email: string;
+}
 
-// let contactInfo: HasEmail | HasPhoneNumber =
-//   Math.random() > 0.5
-//     ? {
-//         // we can assign it to a HasPhoneNumber
-//         name: "Mike",
-//         phone: 3215551212
-//       }
-//     : {
-//         // or a HasEmail
-//         name: "Mike",
-//         email: "mike@example.com"
-//       };
+let contactInfo: HasEmail | HasPhoneNumber =
+  Math.random() > 0.5
+    ? {
+        // we can assign it to a HasPhoneNumber
+        name: "Mike",
+        phone: 3215551212
+      }
+    : {
+        // or a HasEmail
+        name: "Mike",
+        email: "mike@example.com"
+      };
 
-// contactInfo.name; // NOTE: we can only access the .name property  (the stuff HasPhoneNumber and HasEmail have in common)
+contactInfo.name; // NOTE: we can only access the .name property  (the stuff HasPhoneNumber and HasEmail have in common)
 
 /**
  * (15) Intersection types
  */
-// let otherContactInfo: HasEmail & HasPhoneNumber = {
-//   // we _must_ initialize it to a shape that's asssignable to HasEmail _and_ HasPhoneNumber
-//   name: "Mike",
-//   email: "mike@example.com",
-//   phone: 3215551212
-// };
+let otherContactInfo: HasEmail & HasPhoneNumber = {
+  // we _must_ initialize it to a shape that's asssignable to HasEmail _and_ HasPhoneNumber
+  name: "Mike",
+  email: "mike@example.com",
+  phone: 3215551212
+};
 
-// otherContactInfo.name; // NOTE: we can access anything on _either_ type
-// otherContactInfo.email;
-// otherContactInfo.phone;
-// const zzz: any = {} as never;
+otherContactInfo.name; // NOTE: we can access anything on _either_ type
+otherContactInfo.email;
+otherContactInfo.phone;
+const zzz: any = {} as never;
 
 export default {};
